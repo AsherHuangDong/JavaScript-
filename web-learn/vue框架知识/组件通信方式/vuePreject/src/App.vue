374d0c2755bf6110f4ed37f36ee09118c1c63fbd
黄东 <huangdong@huangdongdeMBP.lan>
2021-03-16 10:47:43 +0800

    video

--- web-learn/vue框架知识/组件通信方式/vuePreject/src/App.vue
+++ web-learn/vue框架知识/组件通信方式/vuePreject/src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <h2>name的值为：{{theme.name}}</h2>
-    <button @click="changeName">改变name的值为</button>
-    <index></index>
-    <child @titleChanged="updateTitle" ref="comA"></child>
-    <h2>{{title}}</h2>
-    <parent :users="users"></parent>
+    <!-- <canvas id="canvas"></canvas> -->
+    <article>
+      <img :src="src" alt />
+      <audio src></audio>
+    </article>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import parent from "./components/parent";
-import child from "./components/child";
-import index from "./index";
 export default {
   name: "App",
   // provide(){
@@ -23,45 +18,71 @@ export default {
   //  }
   // },
   // 方法二:使用2.6最新API Vue.observable 优化响应式 provide
-  provide () {
-    this.theme = Vue.observable({
-      name: "jam"
-    });
-    return {
-      theme: this.theme
-    };
-  },
   data () {
     return {
-      //name: 'jam',
-      users: ["Henry", "Bucky", "Emily"],
-      title: "传递的是一个值"
+      src: ''
     };
   },
-  components: {
-    parent,
-    child,
-    index
-  },
   methods: {
-    updateTitle (e) {
-      this.title = e;
-    },
-    changeName () {
-      //this.name = 'food'
-      this.theme.name = "hello";
+    getCanvas () {
+      // let c = document.getElementById('canvas');
+      // let ctx = c.getContext("2d");
+      // let img = new Image();
+      let i = 1
+      let interval = setInterval(() => {
+        if (i === 402) {
+          clearInterval(interval)
+          i = 1
+        }
+        // let m = i
+        // if (m < 10) {
+        //   m = '00' + m
+        // } else if (m < 100) {
+        //   m = '0' + m
+        // } else {
+        //   m = m
+        // }
+        this.src = 'http://192.168.1.238:8080/test1/' + i + '.png';
+        // ctx.height = ctx.height
+        // ctx.drawImage(img, 0, 0);
+        // console.log(img.src)
+        i++
+      }, 66.667)
     }
   },
   mounted () {
-    console.log(this.$refs);
-    const comA = this.$refs.comA;
-    console.log(comA);
-    // //comA.sayHello();
-    // console.log(comA.title);
-    comA.sayHello();
+    this.getCanvas()
   }
 };
 </script>
 
 <style>
+article {
+  width: 3180px;
+  height: 2160px;
+  position: relative;
+}
+img {
+  position: absolute;
+  z-index: 0;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+article::before {
+  content: '播放';
+  width: 200px;
+  height: 200px;
+  background: black;
+  color: aliceblue;
+  position: absolute;
+  font-size: 20px;
+  line-height: 30px;
+  font-weight: 500;
+  z-index: 10;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
